a910f98e8e965927732fefef547c013b00693fd7
rinceoir <voronin.adm@mail.ru>
2023-05-15 14:01:11 +0300

    demo counter props and events

--- demo/src/components/DemoContainer.vue
+++ demo/src/components/DemoContainer.vue
@@ -14,12 +14,14 @@
   align-items: center;
   height: 20rem;
 }
+
 .link {
   border-bottom: 1px solid #343838FF;
   color: #343838FF;
   padding-bottom: .25rem;
   text-decoration: none;
 }
+
 .link_container {
   margin-left: 2rem;
   margin-top: 2rem;
--- demo/src/demo1/DemoCounter.vue
+++ demo/src/demo1/DemoCounter.vue
@@ -1,15 +1,19 @@
 <template>
   <demo-container>
     <div class="counter">
-      <button @click="decrement">-</button>
-      <span>Current value: {{ count }}</span>
-      <button @click="increment">+</button>
+      <counter-button @action="decrement">-</counter-button>
+
+      <counter-value :value="count" />
+
+      <counter-button @action="increment">+</counter-button>
     </div>
   </demo-container>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import CounterButton from "./CounterButton.vue";
+import CounterValue from "./CounterValue.vue";
 import DemoContainer from '../components/DemoContainer.vue';
 
 const count = ref(0);
@@ -27,21 +31,5 @@ const increment = () => {
 .counter {
   display: flex;
   align-items: center;
-
-  button {
-    margin: 0 10px;
-    font-size: 1.5rem;
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    border: none;
-    background: #dd0000;
-    color: white;
-    cursor: pointer;
-
-    &:hover {
-      background: #ff0000;
-    }
-  }
 }
 </style>
