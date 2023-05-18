5f2f7f06d2419c13b8fb1e3324433aef233e0b2e
rinceoir <voronin.adm@mail.ru>
2023-05-18 23:49:27 +0300

    updating

--- frontend/src/common/helpers.js
+++ frontend/src/common/helpers.js
@@ -2,6 +2,8 @@ import { DAY_IN_MILLISEC, TAG_SEPARATOR } from "./constants";
 import timeStatuses from "./enums/timeStatuses";
 import taskStatuses from "./enums/taskStatuses";
 
+import { toRaw } from "vue";
+
 /**
  * принимает строку с метками и разделять её на массив по определённому идентификатору
  * @param {*} tags
@@ -41,4 +43,32 @@ export const normalizeTask = (task) => {
     status: task.statusId ? taskStatuses[task.statusId] : "",
     timeStatus: getTimeStatus(task.dueDate),
   };
+};
+
+export const getImage = (image) => {
+  // https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+  return new URL(`../assets/img/${image}`, import.meta.url).href;
+};
+
+export const getTargetColumnTasks = (toColumnId, tasks) => {
+  return tasks
+    .filter((task) => task.columnId === toColumnId)
+    .map((task) => toRaw(task));
+};
+
+export const addActive = (active, toTask, tasks) => {
+  const activeIndex = tasks.findIndex((task) => task.id === active.id);
+  if (~activeIndex) {
+    tasks.splice(activeIndex, 1);
+  }
+
+  tasks.sort((a, b) => a.sortOrder - b.sortOrder);
+
+  if (toTask) {
+    const toTaskIndex = tasks.findIndex((task) => task.id === toTask.id);
+    tasks.splice(toTaskIndex, 0, active);
+  } else {
+    tasks.push(active);
+  }
+  return tasks;
 };
